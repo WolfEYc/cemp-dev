81a34d82c6c9ac123a5b2e5aface867f45a887f5
Isaac Wolf <isaacwolfsite@gmail.com>
2026-04-03 19:18:31 +0000

    daily promo

---
+++ dbt/cemp/models/gold/daily_promoter_score.sql
@@ -0,0 +1,26 @@
+
+{{ 
+    config(
+        materialized='incremental',
+        unique_key='row_hash',
+        cluster_by=['event_date']
+    )
+}}
+
+select 
+    current_timestamp() AS pipeline_event_ts,
+    hash(DATE(event_ts), vendor, product_id, country, state, referral_type, referral_id) as row_hash,
+    DATE(event_ts) as event_date,
+    vendor,
+    product_id,
+    country,
+    state,
+    referral_type,
+    referral_id,
+    (count_if(score >= 9) / count(*)) - (count_if(score <= 6) / count(*)) as nps,
+    count(*) as num_entries
+from {{ ref('promoter_score') }}
+{% if is_incremental() %}
+    where DATE(event_ts) >= DATEADD(DAY, -7, CURRENT_DATE())
+{% endif %}
+group by DATE(event_ts), vendor, product_id, country, state, referral_type, referral_id
