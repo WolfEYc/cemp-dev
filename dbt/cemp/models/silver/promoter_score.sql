3d886e2c942ce1fdcb4abbf1083ed2e63abed2e1
Isaac Wolf <isaacwolfsite@gmail.com>
2026-04-03 16:37:06 +0000

    add country and other fields

--- dbt/cemp/models/silver/promoter_score.sql
+++ dbt/cemp/models/silver/promoter_score.sql
@@ -4,7 +4,7 @@
     config(
         materialized='incremental',
         unique_key='event_id',
-        cluster_by=['DATE(event_ts)', 'vendor']
+        cluster_by=['DATE(event_ts)', 'vendor', 'product_id', 'country', 'state']
     )
 }}
 
@@ -16,12 +16,18 @@ select
     product_id,
     event_id,
     event_ts,
+    country,
+    state,
+    referral_type,
+    referral_id,
     score
 from bronze.promoter_score
 where 
     event_id is not null
 and event_ts is not null
 and score is not null
+and product_id is not null
+and country is not null
 {% if is_incremental() %}
     and ingest_ts >= (select max(ingest_ts) from {{ this }})
 {% endif %}
